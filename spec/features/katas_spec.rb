00eb8e9b516c196c27cca9ff045505c21a7e6849
Vollcode <daniel.ortiz.olivares@gmail.com>
2018-01-23 19:13:57 +0100

    list katas

--- spec/features/katas_spec.rb
+++ spec/features/katas_spec.rb
@@ -2,16 +2,26 @@ require 'rails_helper'
 require 'capybara'
 
 describe 'Kata' do
+
   it 'can be read' do
-    kata_title = 'La kata suprema'
-    kata_description = 'Kata mejor que la doom'
-    kata = Kata.new(title: kata_title, description: kata_description)
-    kata.save
+    second_title = 'La kata peor'
+    second_description = 'Kata peor que la doom'
+    first_kata = create_kata
+    first_kata.save
+    second_kata = create_kata(title: second_title, description: second_description)
+    second_kata.save
 
     visit root_path
-    click_on(kata_title)
+    click_on(second_title)
 
-    expect(page).to have_content(kata_title)
-    expect(page).to have_content(kata_description)
+    expect(page).to have_content(second_title)
+    expect(page).to have_content(second_description)
   end
+end
+
+def create_kata(title: 'Kata title', description: 'Super Kata')
+  kata = Kata.new(title: title, description: description)
+  kata.save
+
+  kata
 end
